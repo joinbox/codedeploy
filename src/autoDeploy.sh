bf66c9aaad7fbb698503896464d64da6733f4e23
em <michael@joinbox.com>
2016-01-27 15:37:56 +0100

    first working version

--- src/autoDeploy.sh
+++ src/autoDeploy.sh
@@ -11,13 +11,13 @@ remote="git@github.com:$vendor/$repository.git"
 
 
 # check parameters
-if [ vendor == "" ]; then
+if [ -z "$1" ]; then
     echo "parameter 1 to the the deployr command must be the vendor used as on github!"
     exit 1;
 fi
 
 # check parameters
-if [ repository == "" ]; then
+if [ -z "$2" ]; then
     echo "parameter 2 to the the deployr command must be the repository used as on github!"
     exit 1;
 fi
@@ -27,93 +27,108 @@ fi
 
 
 # check out a fresh copy
-mkdir -p "$HOME/apps/$vendor/$repository/master"
-cd "$HOME/apps/$vendor/$repository/master"
-git clone "$remote" ./
+masterPath="$HOME/apps/$vendor/$repository/master"
+
+if [ ! -d "$masterPath" ]; then
+    echo -e "\e[92mCloning into $masterPath\033[0m"
+    mkdir -p "$masterPath"
+    cd "$masterPath"
+    git clone "$remote" ./
+else
+    cd "$masterPath"
+fi
 
 
 
 # get all branches as array
-IFS=', ' read -r -a branches <<< $(git branch | cut -c 3-)
+read -a branches <<<$(git branch -r | cut -c 10- | tail -n +2)
 
 # loop over all branches
 for branch in "${branches[@]}"; do
-    branchPath="$HOME/apps/$vendor/$repository/$branch"
-    serviceName=$("$vendor_$repository_$branch" | sed -r 's/([a-z]+)_([a-z])([a-z]+)/\1\U\2\L\3/')
 
-    # dont check the files for master, we're there already
-    if [ $branch != "master" ]; then
-        echo "Checking branch $vendor/$repository:$branch ..."
+    cd "$masterPath"
+
+    # check if the branch wants to be deployed
+    git cat-file -e "origin/$branch:.deploy/deploy.sh" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+
+        # define paths
+        branchPath="$HOME/apps/$vendor/$repository/$branch"
+        repoPath="$HOME/apps/$vendor/$repository"
+        serviceName=$(echo "$vendor_$repository_$branch" | sed -r 's/([a-z]+)_([a-z])([a-z]+)/\1\U\2\L\3/')
+
+        
+
+        echo -e "\e[92mChecking FOR branch $vendor/$repository:$branch ..."
 
         # create directories, clone git if required
-        if [ ! -d "$branchPath" ]; then                
-            echo "Branch $branch was not checked out before, checking out now ..."
+        if [ ! -d "$branchPath" ]; then
+            echo -e "\e[92mBranch $branch was not cloned before, checking out now ...\033[0m"
             
             mkdir -p "$branchPath"
+            cd "$repoPath"
+            git clone -b "$branch" "$remote" "./$branch"
+            cd "$branchPath" 
+        else
+            echo -e "\e[92mUpdating branch $branch ...\033[0m"
+            # update git
             cd "$branchPath"
-            git clone "$remote" ./
-            git checkout "$branch"
-        fi
-    fi
-
-
-
-    # update git
-    cd "$branchPath"
-    git checkout "$branch"
-    git pull origin "$branch"
-
-
-
-
-
-    # execute install script if present
-    if [ -f "$branchPath/.autorelease.staging.sh" ]; then
-        echo "Executing the .autorelease.staging.sh file ..."
-        source "$branchPath/.autorelease.staging.sh"
-    fi
-
-
-
-
-
-    # check if we need to setup a nginx config
-    if [ -f "$branchPath/.nginx.staging.conf" ]; then
-        nginxConfig="/etc/nginx/site_available/$serviceName.conf"
-
-        if [ ! -f "$nginxConfig" ]; then
-            echo "Installing the nginx config file ..."
-            cp "$branchPath/.nginx.staging.conf" "$nginxConfig"
-
-            # enable site
-            ln -s "$nginxConfig" "/etc/nginx/sites_enabled/$serviceName.conf"
+            git pull origin "$branch"
         fi
 
-        # make sure nginx is running
-        source "$scriptDir/assertNginx.sh"
-
-        # reload
-        service nginx reload
-    fi
+       
+       
 
 
 
-
-
-    # check if we need to setup a system service
-    if [ -f "$branchPath/.service.staging.conf" ]; then
-        serviceScript="/etc/init/$serviceName.conf"
-
-        # do we need to install the service?
-        if [ ! -f "$serviceScript" ]; then
-            echo "Installing the service $serviceName ..."
-            cp "$branchPath/.service.staging.conf" "$serviceScript"
+        # execute install script if present
+        if [ -f "$branchPath/.autorelease.staging.sh" ]; then
+            echo -e "\e[92mExecuting the .autorelease.staging.sh file ...\033[0m"
+            source "$branchPath/.autorelease.staging.sh"
         fi
 
-        # stop the service
-        stop "$serviceName"
 
-        ## start service
-        start "$serviceName"
+
+
+
+        # check if we need to setup a nginx config
+        if [ -f "$branchPath/.nginx.staging.conf" ]; then
+            nginxConfig="/etc/nginx/site_available/$serviceName.conf"
+
+            if [ ! -f "$nginxConfig" ]; then
+                echo "\e[92mInstalling the nginx config file ...\033[0m"
+                cp "$branchPath/.nginx.staging.conf" "$nginxConfig"
+
+                # enable site
+                ln -s "$nginxConfig" "/etc/nginx/sites_enabled/$serviceName.conf"
+            fi
+
+            # make sure nginx is running
+            source "$scriptDir/assertNginx.sh"
+
+            # reload
+            service nginx reload
+        fi
+
+
+
+
+
+        # check if we need to setup a system service
+        if [ -f "$branchPath/.service.staging.conf" ]; then
+            serviceScript="/etc/init/$serviceName.conf"
+
+            # do we need to install the service?
+            if [ ! -f "$serviceScript" ]; then
+                echo -e "\e[92mInstalling the service $serviceName ...\033[0m"
+                cp "$branchPath/.service.staging.conf" "$serviceScript"
+            fi
+
+            # stop the service
+            stop "$serviceName"
+
+            ## start service
+            start "$serviceName"
+        fi
     fi
 done
